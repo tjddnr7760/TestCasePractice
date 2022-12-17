938766512d238cff1f97c5a98ffc74b3ae7b89a0
cs2199 <tjddnr7760@naver.com>
2022-12-17 16:07:58 +0900

    test(PasswordSecurityCheckerTest) : 숫자를 포함하지 않고 나머지 테스트는 통과하는 테스트 케이스 추가, 구현, check함수 리팩터링

--- src/test/java/PasswordSecurityChecker.java
+++ src/test/java/PasswordSecurityChecker.java
@@ -1,8 +1,25 @@
 public class PasswordSecurityChecker {
     public SecurityLevel check(String word) {
-        if(word.length() < 8) {
+        if (isShort(word) || !containsNumber(word)) {
             return SecurityLevel.NORMAL;
         }
-        return  SecurityLevel.STRONG;
+        return SecurityLevel.STRONG;
+    }
+
+    private boolean isShort(String word) {
+        return word.length() < 8;
+    }
+
+    private boolean containsNumber(String word) {
+        for (char ch : word.toCharArray()) {
+            if (isDigit(ch)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isDigit(char ch) {
+        return ch >= '0' && ch <= '9';
     }
 }
--- src/test/java/PasswordSecurityCheckerTest.java
+++ src/test/java/PasswordSecurityCheckerTest.java
@@ -2,15 +2,15 @@
  *  요구사항 정리
  *
  *  개요 : 문자열을 검사해서 규칙을 준수하는지에 따라 암호를 '약함', '보통', '강함'
- *  으로 구분한다.
- *
- *  규칙 :   1. 길이가 8글자 이상
- *          2. 0 ~ 9 사이의 숫자
- *          3. 대문자 포함
- *
- *  판단 :   1. 3개의 규칙을 모두 충족하면 암호는 강함이다.
- *          2. 2개의 규칙을 충족하면 암호는 보통이다.
- *          3. 1개의 규칙을 충족하면 암호는 약함이다.
+ *  으로 구분한다. 규칙을 충족하면 암호는 보통이다.
+ *          3. 1개의
+ *  *
+ *  *  규칙 :   1. 길이가 8글자 이상
+ *  *          2. 0 ~ 9 사이의 숫자를 포함
+ *  *          3. 대문자 포함
+ *  *
+ *  *  판단 :   1. 3개의 규칙을 모두 충족하면 암호는 강함이다.
+ *  *          2. 2개의 규칙을 충족하면 암호는 약함이다.
  *
  */
 import org.junit.jupiter.api.Test;
@@ -35,5 +35,12 @@ public class PasswordSecurityCheckerTest {
 
         SecurityLevel second_level = checker.check("Ab12!c");
         assertEquals(SecurityLevel.NORMAL, second_level);
+    }
+
+    @Test
+    void is_passAllTestExceptNumberConditions_Normal() {
+        PasswordSecurityChecker checker = new PasswordSecurityChecker();
+        SecurityLevel level = checker.check("ab!@ABqwer");
+        assertEquals(SecurityLevel.NORMAL, level);
     }
 }
