407f036913e642732a499c4900a75e39cefc9b1a
cs2199 <tjddnr7760@naver.com>
2022-12-17 11:50:55 +0900

    test(PasswordStrengthMeter) : 패스워드강도측정 테스트 클래스 생성

---
+++ src/test/java/PasswordStrengthMeter.java
@@ -0,0 +1,24 @@
+/**
+ *  요구사항 정리
+ *
+ *  개요 : 문자열을 검사해서 규칙을 준수하는지에 따라 암호를 '약함', '보통', '강함'
+ *  으로 구분한다.
+ *
+ *  규칙 :   1. 길이가 8글자 이상
+ *          2. 0 ~ 9 사이의 숫자
+ *          3. 대문자 포함
+ *
+ *  판단 :   1. 3개의 규칙을 모두 충족하면 암호는 강함이다.
+ *          2. 2개의 규칙을 충족하면 암호는 보통이다.
+ *          3. 1개의 규칙을 충족하면 암호는 약함이다.
+ *
+ */
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PasswordStrengthMeter {
+    @Test
+    void run(){
+
+    }
+}
