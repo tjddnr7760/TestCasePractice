19026cc03605af396d8a12b0f82a2b7860174c8a
cs2199 <tjddnr7760@naver.com>
2022-12-18 18:17:53 +0900

    feat(PaymentExpirationCalculator) : 테스트를 통과하는 가장 간단한 함수 구현

--- src/test/java/PaymentExpirationCalculator.java
+++ src/test/java/PaymentExpirationCalculator.java
@@ -1,3 +1,7 @@
-public class PaymentExpirationCalculator {
+import java.time.LocalDate;
 
+public class PaymentExpirationCalculator {
+    public LocalDate expiaryDate(LocalDate date, int moneyAmount) {
+        return LocalDate.of(2019, 4, 1);
+    }
 }
