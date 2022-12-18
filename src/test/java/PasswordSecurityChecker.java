d6ca6276c09001a182ff136eba74d744365e6ae7
cs2199 <tjddnr7760@naver.com>
2022-12-18 15:58:31 +0900

    refactor(PasswordSecurityChecker) : 함수 분리

--- src/test/java/PasswordSecurityChecker.java
+++ src/test/java/PasswordSecurityChecker.java
@@ -4,29 +4,32 @@ public class PasswordSecurityChecker {
             return SecurityLevel.INVALID;
         }
 
-        int conditionCounts = 0;
-
-        if(containsUppercaseLetters(word)) {
-            conditionCounts++;
-        }
-
-        if(isLength(word)) {
-            conditionCounts++;
-        }
-
-        if(containsNumber(word)) {
-            conditionCounts++;
-        }
+        int conditionCounts = checkConditionsCount(word);
 
         if(conditionCounts <= 1) {
             return SecurityLevel.WEAK;
         }
-
         if (conditionCounts == 2) {
             return SecurityLevel.NORMAL;
         }
 
         return SecurityLevel.STRONG;
+    }
+
+    private int checkConditionsCount(String word) {
+        int conditionCounts = 0;
+
+        if(containsUppercaseLetters(word)) {
+            conditionCounts++;
+        }
+        if(isLength(word)) {
+            conditionCounts++;
+        }
+        if(containsNumber(word)) {
+            conditionCounts++;
+        }
+
+        return conditionCounts;
     }
 
     private boolean containsUppercaseLetters(String word) {
