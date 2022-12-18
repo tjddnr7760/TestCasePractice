652c71748f0ed8db8e494b91188e43e9e5e1f1ed
cs2199 <tjddnr7760@naver.com>
2022-12-18 18:38:52 +0900

    feat(PaymentExpirationCalculator) : 테스트 케이스 추가하면서 범용화된 함수 구현

--- src/test/java/PaymentExpirationCalculator.java
+++ src/test/java/PaymentExpirationCalculator.java
@@ -2,6 +2,6 @@ import java.time.LocalDate;
 
 public class PaymentExpirationCalculator {
     public LocalDate expiaryDate(LocalDate date, int moneyAmount) {
-        return LocalDate.of(2019, 4, 1);
+        return date.plusMonths(1);
     }
 }
