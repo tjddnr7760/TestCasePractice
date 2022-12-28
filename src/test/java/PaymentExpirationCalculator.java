e81dd8d47e115e6a9caa2090defe2147154fdf3b
cs2199 <tjddnr7760@naver.com>
2022-12-28 11:55:33 +0900

    feat(PaymentExpirationCalculator) : 2만원 이상 납부시 n달 이후 만료일 리턴함수 구현

--- src/test/java/PaymentExpirationCalculator.java
+++ src/test/java/PaymentExpirationCalculator.java
@@ -2,7 +2,7 @@ import java.time.LocalDate;
 
 public class PaymentExpirationCalculator {
     public LocalDate expiaryDate(PayData paydata) {
-        int addedMonths = 1;
+        int addedMonths = paydata.getMoneyAmount() / 10_000;
         if(paydata.getFirstBillingDate() != null) {
             LocalDate candidate = paydata.getLocaldate().plusMonths(addedMonths);
             if(candidate.getDayOfMonth() != paydata.getFirstBillingDate().getDayOfMonth()) {
