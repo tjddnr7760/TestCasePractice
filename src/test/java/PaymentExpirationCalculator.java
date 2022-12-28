fdeec73badec853c76c43eb80b87f0eb7144e118
cs2199 <tjddnr7760@naver.com>
2022-12-28 11:44:21 +0900

    refactor(PaymentExpirationCalculator) : 상수 변수화

--- src/test/java/PaymentExpirationCalculator.java
+++ src/test/java/PaymentExpirationCalculator.java
@@ -2,12 +2,13 @@ import java.time.LocalDate;
 
 public class PaymentExpirationCalculator {
     public LocalDate expiaryDate(PayData paydata) {
+        int addedMonths = 1;
         if(paydata.getFirstBillingDate() != null) {
-            LocalDate candidate = paydata.getLocaldate().plusMonths(1);
+            LocalDate candidate = paydata.getLocaldate().plusMonths(addedMonths);
             if(candidate.getDayOfMonth() != paydata.getFirstBillingDate().getDayOfMonth()) {
                 return candidate.withDayOfMonth(paydata.getFirstBillingDate().getDayOfMonth());
             }
         }
-        return paydata.getLocaldate().plusMonths(1);
+        return paydata.getLocaldate().plusMonths(addedMonths);
     }
 }
