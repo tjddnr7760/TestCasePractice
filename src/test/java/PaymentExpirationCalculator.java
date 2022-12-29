e80fed43517c5f826a00ed3a818cc5bb8b6b07e9
cs2199 <tjddnr7760@naver.com>
2022-12-29 09:42:51 +0900

    feat(PaymentExpirationCalculator) : 첫지불일과 현재 지불일의 날짜가 다르고 2만원 이상 납부할 경우 테스트 추가 사항에 맞게 함수 구현

--- src/test/java/PaymentExpirationCalculator.java
+++ src/test/java/PaymentExpirationCalculator.java
@@ -1,11 +1,15 @@
 import java.time.LocalDate;
+import java.time.YearMonth;
 
 public class PaymentExpirationCalculator {
     public LocalDate expiaryDate(PayData paydata) {
         int addedMonths = paydata.getMoneyAmount() / 10_000;
-        if(paydata.getFirstBillingDate() != null) {
+        if (paydata.getFirstBillingDate() != null) {
             LocalDate candidate = paydata.getLocaldate().plusMonths(addedMonths);
-            if(candidate.getDayOfMonth() != paydata.getFirstBillingDate().getDayOfMonth()) {
+            if (candidate.getDayOfMonth() != paydata.getFirstBillingDate().getDayOfMonth()) {
+                if (YearMonth.from(candidate).lengthOfMonth() < paydata.getFirstBillingDate().getDayOfMonth()) {
+                    return candidate.withDayOfMonth(YearMonth.from(candidate).lengthOfMonth());
+                }
                 return candidate.withDayOfMonth(paydata.getFirstBillingDate().getDayOfMonth());
             }
         }
