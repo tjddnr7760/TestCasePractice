b9f85db1a3975a63123a56b6c3b7ec4bb8782ede
cs2199 <tjddnr7760@naver.com>
2022-12-29 11:03:30 +0900

    refactor(PaymentExpirationCalculator) : 중복제거, 메소드 분리

--- src/test/java/PaymentExpirationCalculator.java
+++ src/test/java/PaymentExpirationCalculator.java
@@ -5,14 +5,24 @@ public class PaymentExpirationCalculator {
     public LocalDate expiaryDate(PayData paydata) {
         int addedMonths = paydata.getMoneyAmount() / 10_000;
         if (paydata.getFirstBillingDate() != null) {
-            LocalDate candidate = paydata.getLocaldate().plusMonths(addedMonths);
-            if (candidate.getDayOfMonth() != paydata.getFirstBillingDate().getDayOfMonth()) {
-                if (YearMonth.from(candidate).lengthOfMonth() < paydata.getFirstBillingDate().getDayOfMonth()) {
-                    return candidate.withDayOfMonth(YearMonth.from(candidate).lengthOfMonth());
-                }
-                return candidate.withDayOfMonth(paydata.getFirstBillingDate().getDayOfMonth());
-            }
+            return expiryDateUsingFirstDate(paydata, addedMonths);
         }
-        return paydata.getLocaldate().plusMonths(addedMonths);
+        else {
+            return paydata.getLocaldate().plusMonths(addedMonths);
+        }
+    }
+
+    private LocalDate expiryDateUsingFirstDate(PayData paydata, int addedMonths) {
+        LocalDate candidate = paydata.getLocaldate().plusMonths(addedMonths);
+        final int dayOfMonthFirstBilling = paydata.getFirstBillingDate().getDayOfMonth();
+        if (candidate.getDayOfMonth() != dayOfMonthFirstBilling) {
+            final int dayOfMonthCandidate = YearMonth.from(candidate).lengthOfMonth();
+            if (dayOfMonthCandidate < dayOfMonthFirstBilling) {
+                return candidate.withDayOfMonth(dayOfMonthCandidate);
+            }
+            return candidate.withDayOfMonth(dayOfMonthFirstBilling);
+        } else {
+            return candidate;
+        }
     }
 }
