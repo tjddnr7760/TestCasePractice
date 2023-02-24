b1e5055a49d9ffa929f48cf84ac7b14138c0ac4e
cs2199 <tjddnr7760@naver.com>
2023-02-24 17:21:12 +0900

    test : asList 예시

--- src/test/java/AssertjPractice.java
+++ src/test/java/AssertjPractice.java
@@ -4,6 +4,9 @@ import org.assertj.core.api.InstanceOfAssertFactories;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class AssertjPractice {
 
     @DisplayName("assertThat isInstanceOf 체이닝 메서드 예시")
@@ -27,4 +30,15 @@ public class AssertjPractice {
         assertThat(obj).asInstanceOf(InstanceOfAssertFactories.INTEGER).isEqualTo(123);
     }
 
+    @DisplayName("asList 메서드 예시")
+    @Test
+    void test_asList() {
+        //given
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+
+        //when, then
+        System.out.println("1 " + assertThat(list).contains(1));
+        System.out.println("2 " + assertThat(list).asList().contains(1));
+        //asList 를 굳이 사용하지 않아도 되지만 가독성을 위해 사용한다고 이해한다.
+    }
 }
