0c2a85aa9c748e4a64b1f372e596999562b15763
cs2199 <tjddnr7760@naver.com>
2023-02-24 16:44:20 +0900

    test : asInstanceOf 예시

--- src/test/java/AssertjPractice.java
+++ src/test/java/AssertjPractice.java
@@ -1,13 +1,14 @@
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.assertj.core.api.InstanceOfAssertFactories;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class AssertjPractice {
 
-    @DisplayName("assertThat instanceOf 체이닝 메서드 예시")
+    @DisplayName("assertThat isInstanceOf 체이닝 메서드 예시")
     @Test
-    void test_instanceOf() {
+    void test_isInstanceOf() {
         //given
         String str = "Hello!";
 
@@ -15,4 +16,15 @@ public class AssertjPractice {
         assertThat(str).isInstanceOf(Object.class);
         //assertThat(str).isInstanceOf(Integer.class);
     }
+
+    @DisplayName("asInstanceOf 메서드 예시")
+    @Test
+    void test_asInstanceOf() {
+        //given
+        Object obj = 123;
+
+        //when, then
+        assertThat(obj).asInstanceOf(InstanceOfAssertFactories.INTEGER).isEqualTo(123);
+    }
+
 }
