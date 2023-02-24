5f58435722e9e452ac9db650f794771ed84d1ac5
cs2199 <tjddnr7760@naver.com>
2023-02-24 16:16:34 +0900

    test : instanceOf 메서드

--- src/test/java/AssertjPractice.java
+++ src/test/java/AssertjPractice.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class AssertjPractice {
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class AssertjPractice {
+
+    @DisplayName("assertThat instanceOf 체이닝 메서드 예시")
+    @Test
+    void test_instanceOf() {
+        //given
+        String str = "Hello!";
+
+        //when, then
+        assertThat(str).isInstanceOf(Object.class);
+        //assertThat(str).isInstanceOf(Integer.class);
+    }
 }
